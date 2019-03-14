2fac76ba6e784ec94148ae48c42989ab91ef9272
yukuriya3 <yukuriya3.kobe@gmail.com>
2019-03-14 18:56:53 +0900

    model.py and simulation.py update

--- THP_pro_pathway_select/model.py
+++ THP_pro_pathway_select/model.py
@@ -18,7 +18,7 @@ def model1(u, t, k0, Vmax1, KmA, Vmax2, KmB, Kmo2, O2, k3, k4, Inh, KiB, KiC):
     
     
     
-# model2: DDC + MAO pathway, +DHPAA drain, + amines inhibition
+# model2: DDC + MAO pathway, +DHPAA drain, +product inhibition
 def model2(u, t, k0, Vmax1, KmA, Vmax2, KmB, Kmo2, O2, k3, k4, Inh, KiB, KiC, k5):
     
     A = u[0] # Dopa
@@ -34,30 +34,10 @@ def model2(u, t, k0, Vmax1, KmA, Vmax2, KmB, Kmo2, O2, k3, k4, Inh, KiB, KiC, k5
     dDdt = k3 * B * C - k4 * D
     dSdt = k0 * (1.0 - pow(S/100, 30))
     dPdt = k3 * B * C
-    
     return([dAdt, dBdt, dCdt, dDdt, dSdt, dPdt])
 
-# model3: DDC + MAOS pathway, +DHPAA drain, no amines inhibition
-def model3(u, t, k0, Vmax1, KmA, Vmax2, KmB, Kmo2, O2, k3, k4, k5):
-    
-    A = u[0] # Dopa
-    B = u[1] # Dopamin
-    C = u[2] # DHPAA
-    D = u[3] # THP
-    S = u[4] # Total fed Dopa
-    P = u[5] # Total converted THP
-    
-    dAdt = k0 * (1.0 - pow(S/100, 30)) - Vmax1 * A / (KmA + A)
-    dBdt = Vmax1 * A / (KmA + A) - Vmax2 * B * O2 / ((KmB + B) * (Kmo2 + O2)) - k3 * B * C
-    dCdt = Vmax2 * B * O2 / ((KmB + B) * (Kmo2 + O2)) - k3 * B * C - k5 * C
-    dDdt = k3 * B * C - k4 * D
-    dSdt = k0 * (1.0 - pow(S/100, 30))
-    dPdt = k3 * B * C
-    
-    return([dAdt, dBdt, dCdt, dDdt, dSdt, dPdt])
-
-# model4: DDC + DHPAAS pathway, no DHPAA drain, +product inhibition
-def model4(u, t, k0, Vmax1, KmA, Vmax2, KmA2, Kmo2, O2, k3, k4, KiB, KiC):
+# model3: DDC + DHPAAS pathway, no DHPAA drain, +product inhibition
+def model3(u, t, k0, Vmax1, KmA, Vmax2, KmA2, Kmo2, O2, k3, k4, KiB, KiC):
     
     A = u[0] # Dopa
     B = u[1] # Dopamin
@@ -74,8 +54,8 @@ def model4(u, t, k0, Vmax1, KmA, Vmax2, KmA2, Kmo2, O2, k3, k4, KiB, KiC):
     dPdt = k3 * B * C
     return([dAdt, dBdt, dCdt, dDdt, dSdt, dPdt])
 
-# model5: DDC + DHPAAS pathway, +DHPAA drain, +product inhibition
-def model5(u, t, k0, Vmax1, KmA, Vmax2, KmA2, Kmo2, O2, k3, k4, KiB, KiC, k5):
+# model4: DDC + DHPAAS pathway, +DHPAA drain, +product inhibition
+def model4(u, t, k0, Vmax1, KmA, Vmax2, KmA2, Kmo2, O2, k3, k4, KiB, KiC, k5):
     
     A = u[0] # Dopa
     B = u[1] # Dopamin
@@ -90,24 +70,5 @@ def model5(u, t, k0, Vmax1, KmA, Vmax2, KmA2, Kmo2, O2, k3, k4, KiB, KiC, k5):
     dDdt = k3 * B * C - k4 * D
     dSdt = k0 * (1.0 - pow(S/100, 30))
     dPdt = k3 * B * C
-    return([dAdt, dBdt, dCdt, dDdt, dSdt, dPdt])
-
-# model6: DDC + DHPAAS pathway, +DHPAA drain, no product inhibition
-def model6(u, t, k0, Vmax1, KmA, Vmax2, KmA2, Kmo2, O2, k3, k4, k5):
-    
-    A = u[0] # Dopa
-    B = u[1] # Dopamin
-    C = u[2] # DHPAA
-    D = u[3] # THP
-    S = u[4] # Total fed Dopa
-    P = u[5] # Total converted THP
-    
-    dAdt = k0 * (1.0 - pow(S/100, 30)) - Vmax1 * A / (KmA + A) - Vmax2 * A * O2 / ((KmA2 + A) * (Kmo2 + O2))
-    dBdt = Vmax1 * A / (KmA + A) - k3 * B * C
-    dCdt = Vmax2 * A * O2 / ((KmA2 + A) * (Kmo2 + O2)) - k3 * B * C - k5 * C
-    dDdt = k3 * B * C - k4 * D
-    dSdt = k0 * (1.0 - pow(S/100, 30))
-    dPdt = k3 * B * C
-    
     return([dAdt, dBdt, dCdt, dDdt, dSdt, dPdt])
 
--- THP_pro_pathway_select/simulation.py
+++ THP_pro_pathway_select/simulation.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 # model import
 import model
-func = [model.model1, model.model2, model.model3, model.model4, model.model5, model.model6]
+func = [model.model1, model.model2, model.model3, model.model4]
 
 
 
@@ -24,12 +24,12 @@ p2_pv = []
 DHPAA_Dopamin_ratio = []
 
 
-for number in range(6):
+for number in range(4):
  
  if number == 0:
   seed(103)
   # file open
-  with open('result_DDC_plus_MAO_pathway_wo_DHPAA_drain_wi_inhibition.csv','a',newline='') as f:
+  with open('result_path1_solv10_wo_DHPAA_drain.csv','a',newline='') as f:
    writer = csv.writer(f)
    header = ['k0', 'Vmax1', 'KmA', 'Vmax2', 'KmB', 'Kmo2', 'O2', 'k3', 'k4', 'Inh', 'KiB', 'KiC']
    writer.writerow(header)
@@ -72,7 +72,7 @@ for number in range(6):
     
     
   eval2 = np.c_[eval] / 50 * 100
-  np.savetxt('eval__DDC_plus_MAO_pathway_wo_DHPAA_drain_wi_inhibition.csv', eval2, delimiter=',',header='Eval_index', fmt='%.5f')
+  np.savetxt('eval_solv_10_path1_without_DHPAA_drain.csv', eval2, delimiter=',',header='Eval_index', fmt='%.5f')
   
   df = eval2
   
@@ -90,7 +90,7 @@ for number in range(6):
   
   plt.tight_layout()
   
-  plt.savefig("output_histgram_eval_DDC_plus_MAO_pathway_wo_DHPAA_drain_wi_inhibition.png")
+  plt.savefig("output_histgram_eval_solv_10_path1_without_DHPAA_drain.png")
   #plt.show()
   plt.close()
   
@@ -111,7 +111,7 @@ for number in range(6):
   plt.ylabel('Conversion efficiency from Substrate to Target (%)')
   plt.ylim([0,100])
   
-  plt.savefig("output_boxplot_eval_DDC_plus_MAO_pathway_wo_DHPAA_drain_wi_inhibition.png")
+  plt.savefig("output_boxplot_eval_solv_10_path1_without_DHPAA_drain.png")
   #plt.show()
   plt.close()
   
@@ -122,7 +122,7 @@ for number in range(6):
   seed(103)
   
   # file open
-  with open('result_DDC_plus_MAO_pathway_wi_DHPAA_drain_wi_inhibition.csv','a',newline='') as f:
+  with open('result_path1_solv10_wi_DHPAA_drain.csv','a',newline='') as f:
    writer = csv.writer(f)
    header = ['k0', 'Vmax1', 'KmA', 'Vmax2', 'KmB', 'Kmo2', 'O2', 'k3', 'k4', 'Inh', 'KiB', 'KiC', 'k5']
    writer.writerow(header)
@@ -166,7 +166,7 @@ for number in range(6):
     
   # output of THP yield
   eval2 = np.c_[eval] / 50 * 100
-  np.savetxt('eval_DDC_plus_MAO_pathway_wi_DHPAA_drain_wi_inhibition.csv', eval2, delimiter=',',header='Eval_index', fmt='%.5f')
+  np.savetxt('eval_solv_10_path1_with_DHPAA_drain.csv', eval2, delimiter=',',header='Eval_index', fmt='%.5f')
   
   df1 = eval2
   
@@ -183,7 +183,7 @@ for number in range(6):
   
   plt.tick_params(labelsize=14)
   
-  plt.savefig("output_histgram_eval_DDC_plus_MAO_pathway_wi_DHPAA_drain_wi_inhibition.png")
+  plt.savefig("output_histgram_eval_solv_10_path1_with_DHPAA_drain.png")
   plt.close()
   
   ## output of statistics
@@ -202,107 +202,19 @@ for number in range(6):
   plt.ylabel('Conversion efficiency from Substrate to Target (%)')
   plt.ylim([0,100])
   
-  plt.savefig("output_boxplot_eval_DDC_plus_MAO_pathway_wi_DHPAA_drain_wi_inhibition.png")
+  plt.savefig("output_boxplot_eval_solv_10_path1_with_DHPAA_drain.png")
   #plt.show()
   plt.close()
   
   eval = []
+  
   
  elif number == 2:
-  
-  seed(103)
-  
-  # file open
-  with open('result_DDC_plus_MAO_pathway_wi_DHPAA_drain_wo_inhibition.csv','a',newline='') as f:
-   writer = csv.writer(f)
-   header = ['k0', 'Vmax1', 'KmA', 'Vmax2', 'KmB', 'Kmo2', 'O2', 'k3', 'k4', 'k5']
-   writer.writerow(header)
-   
-   Param_ub = [10.0, 1.0e+4, 10, 1.0e+4, 10, 1.0, 0.236, 1.0, 1.0, 1.0]
-   
-   for var in range(0, 10000):
-    
-    # initial values
-    u0 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    
-    # setting
-    dt = 0.1
-    T = 100.0
-    times = np.arange(0.0, T, dt)
-    
-    Param = Param_ub * rand(10)
-    
-    k0 = Param[0]
-    Vmax1 = Param[1]
-    KmA = Param[2]
-    Vmax2 = Param[3]
-    KmB = Param[4]
-    Kmo2 = Param[5]
-    O2 = Param[6]
-    k3 = Param[7]
-    k4 = Param[8]
-    k5 = Param[9]
-    
-    
-    # numerical solution using scipy.integrate
-    args = (Param[0], Param[1], Param[2], Param[3], Param[4], Param[5], Param[6], Param[7], Param[8], Param[9])
-    orbit = odeint(func[2], u0, times, args)
-    
-    # output of parameter sets
-    writer.writerow(Param)
-    eval.append(orbit[len(orbit)-1, 5])
-    
-  # output of THP yield
-  eval2 = np.c_[eval] / 50 * 100
-  np.savetxt('eval_DDC_plus_MAO_pathway_wi_DHPAA_drain_wo_inhibition.csv', eval2, delimiter=',',header='Eval_index', fmt='%.5f')
-  
-  df2 = eval2
-  
-  
-  # Histgram using matplotlib
-  
-  fig = plt.figure()
-  ax = fig.add_subplot(1,1,1)
-  
-  ax.hist(eval2, bins=50)
-  ax.set_title('Histgram of Conversion efficiency from Substrate to Target (%)', fontsize=14)
-  ax.set_xlabel('Conversion efficiency from Substrate to Target (%)', fontsize=14)
-  ax.set_ylabel('Frequency (-)', fontsize=14)
-  
-  plt.tick_params(labelsize=14)
-  
-  plt.savefig("output_histgram_eval_DDC_plus_MAO_pathway_wi_DHPAA_drain_wo_inhibition.png")
-  plt.close()
-  
-  ## output of statistics
-  print(pd.DataFrame(pd.Series(eval2.ravel()).describe()).transpose())
-  
-  ## Boxplot using matplotlib
-  fig = plt.figure()
-  ax = fig.add_subplot(1,1,1)
-  
-  bp = ax.boxplot(eval2)
-  ax.set_xticklabels(['Path2'])
-  
-  plt.title('Box plot of Conversion efficiency from Substrate to Target (%)')
-  plt.grid()
-  plt.xlabel('Pathway')
-  plt.ylabel('Conversion efficiency from Substrate to Target (%)')
-  plt.ylim([0,100])
-  
-  plt.savefig("output_boxplot_eval_DDC_plus_MAO_pathway_wi_DHPAA_drain_wo_inhibition.png")
-  #plt.show()
-  plt.close()
-  
-  eval = []
-  
-  
- elif number == 3:
   
   seed(110)
   
   # file open
-  with open('result_DDC_plus_DHPAAS_wo_DHPAA_drain_wi_product_inhibition.csv','a',newline='') as f:
+  with open('result_path2_solv10_wo_DHPAA_drain.csv','a',newline='') as f:
    writer = csv.writer(f)
    header = ['k0', 'Vmax1', 'KmA', 'Vmax2', 'KmA2', 'Kmo2', 'O2', 'k3', 'k4', 'KiB', 'KiC']
    writer.writerow(header)
@@ -341,7 +253,7 @@ for number in range(6):
     
     # numerical solution using scipy.integrate
     args = (Param[0], Param[1], Param[2], Param[3], Param[4], Param[5], Param[6], Param[7], Param[8], Param[9], Param[10])
-    orbit = odeint(func[3], u0, times, args)
+    orbit = odeint(func[2], u0, times, args)
     
     # output of parameter sets
     writer.writerow(Param)
@@ -349,9 +261,9 @@ for number in range(6):
     
   # output of THP yield
   eval2 = np.c_[eval] / 50 * 100
-  np.savetxt('eval_DDC_plus_DHPAAS_wo_DHPAA_drain_wi_product_inhibition.csv', eval2, delimiter=',',header='Eval_index', fmt='%.5f')
+  np.savetxt('eval_solv_10_path2_without_DHPAA_drain.csv', eval2, delimiter=',',header='Eval_index', fmt='%.5f')
   
-  df3 =eval2
+  df2 =eval2
   
   # Histgram using matplotlib
   
@@ -365,7 +277,7 @@ for number in range(6):
   
   plt.tick_params(labelsize=14)
   
-  plt.savefig("output_histgram_eval_DDC_plus_DHPAAS_wo_DHPAA_drain_wi_product_inhibition.png")
+  plt.savefig("output_histgram_eval_solv_10_path2_without_DHPAA_drain.png")
   plt.close()
   
   ## output of statistics
@@ -384,18 +296,18 @@ for number in range(6):
   plt.ylabel('Conversion efficiency from Substrate to Target (%)')
   plt.ylim([0,100])
   
-  plt.savefig("output_boxplot_eval_DDC_plus_DHPAAS_wo_DHPAA_drain_wi_product_inhibition.png")
+  plt.savefig("output_boxplot_eval_solv_10_path2_without_DHPAA_drain.png")
   #plt.show()
   plt.close()
   
   eval = []
   
   
- elif number == 4:
+ elif number == 3:
   
   seed(110)
   
-  with open('result_DDC_plus_DHPAAS_wi_DHPAA_drain_wi_product_inhibition.csv','a',newline='') as f:
+  with open('result_path2_solv10_wi_DHPAA_drain.csv','a',newline='') as f:
    writer = csv.writer(f)
    header = ['k0', 'Vmax1', 'KmA', 'Vmax2', 'KmA2', 'Kmo2', 'O2', 'k3', 'k4', 'KiB', 'KiC', 'k5']
    writer.writerow(header)
@@ -443,7 +355,7 @@ for number in range(6):
     
     # numerical solution using scipy.integrate
     args = (Param[0], Param[1], Param[2], Param[3], Param[4], Param[5], Param[6], Param[7], Param[8], Param[9], Param[10], Param[11])
-    orbit = odeint(func[4], u0, times, args)
+    orbit = odeint(func[3], u0, times, args)
     
     
     
@@ -454,9 +366,12 @@ for number in range(6):
   
   # output of THP yield
   eval2 = np.c_[eval] / 50 * 100
-  np.savetxt('eval_DDC_plus_DHPAAS_wi_DHPAA_drain_wi_product_inhibition.csv', eval2, delimiter=',',header='Eval_index', fmt='%.5f')
+  np.savetxt('eval_solv_10_path2_with_DHPAA_drain.csv', eval2, delimiter=',',header='Eval_index', fmt='%.5f')
   
-  df4 = eval2
+  df3 = eval2
+  
+  data = np.concatenate((df, df1, df2, df3), 1)
+  
   
   
   # Histgram using matplotlib
@@ -471,7 +386,7 @@ for number in range(6):
   
   plt.tick_params(labelsize=14)
   
-  plt.savefig("output_histgram_eval_DDC_plus_DHPAAS_wi_DHPAA_drain_wi_product_inhibition.png")
+  plt.savefig("output_histgram_eval_solv_10_path2_with_DHPAA_drain.png")
   #plt.show()
   plt.close()
   
@@ -491,122 +406,14 @@ for number in range(6):
   plt.ylabel('Conversion efficiency from Substrate to Target (%)')
   plt.ylim([0,100])
   
-  plt.savefig("output_boxplot_eval_DDC_plus_DHPAAS_wi_DHPAA_drain_wi_product_inhibition.png")
-  #plt.show()
-  plt.close()
-  
-  eval = []
-
- elif number == 5:
-  
-  seed(213)
-  
-  with open('result_DDC_plus_DHPAAS_wi_DHPAA_drain_wo_product_inhibition.csv','a',newline='') as f:
-   writer = csv.writer(f)
-   header = ['k0', 'Vmax1', 'KmA', 'Vmax2', 'KmA2', 'Kmo2', 'O2', 'k3', 'k4','k5']
-   writer.writerow(header)
-   
-   Param_ub = [10.0, 1.0e+4, 10, 1.0e+4, 10, 1.0, 0.236, 1.0, 100, 1.0]
-   
-   
-   #eval = []
-   #p1_pv = []
-   #p2_pv = []
-   
-   #DHPAA_Dopamin_ratio = []
-   
-   
-   
-   for var in range(0, 10000):
-    #Param_ub = [10.0, 1.0e+4, 10, 1.0e+4, 10, 1.0, 0.236, 1.0, 100, 10.0, 10.0, 1.0]
-    #Param_ub = [100] * 9  # initialization value: 100, elements: 9 
-    #Param_ub[6] = 5.5e-5
-    #Param = Param_ub * rand(12)
-    #Param = [10.0, 8.0/3.0, 28.0]
-    
-    
-    # initial values
-    u0 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    # setting
-    dt = 0.1
-    T = 100.0
-    times = np.arange(0.0, T, dt)
-    
-    Param = Param_ub * rand(10)
-    
-    k0 = Param[0]
-    Vmax1 = Param[1]
-    KmA = Param[2]
-    Vmax2 = Param[3]
-    KmA2 = Param[4]
-    Kmo2 = Param[5]
-    O2 = Param[6]
-    k3 = Param[7]
-    k4 = Param[8]
-    k5 = Param[9]
-    
-    # numerical solution using scipy.integrate
-    args = (Param[0], Param[1], Param[2], Param[3], Param[4], Param[5], Param[6], Param[7], Param[8], Param[9])
-    orbit = odeint(func[5], u0, times, args)
-    
-    
-    
-    # output of parameter sets
-    writer.writerow(Param)
-    eval.append(orbit[len(orbit)-1, 5])
-  
-  
-  # output of THP yield
-  eval2 = np.c_[eval] / 50 * 100
-  np.savetxt('eval_DDC_plus_DHPAAS_wi_DHPAA_drain_wo_product_inhibition.csv', eval2, delimiter=',',header='Eval_index', fmt='%.5f')
-  
-  df5 = eval2
-  
-  data = np.concatenate((df, df1, df2, df3, df4, df5), 1)
-  
-  print(data)
-  
-  # Histgram using matplotlib
-  
-  fig = plt.figure()
-  ax = fig.add_subplot(1,1,1)
-  
-  ax.hist(eval2, bins=50)
-  ax.set_title('Histgram of Conversion efficiency from Substrate to Target (%)', fontsize=14)
-  ax.set_xlabel('Conversion efficiency from Substrate to Target (%)', fontsize=14)
-  ax.set_ylabel('Frequency (-)', fontsize=14)
-  
-  plt.tick_params(labelsize=14)
-  
-  plt.savefig("output_histgram_eval_DDC_plus_DHPAAS_wi_DHPAA_drain_wo_product_inhibition.png")
-  #plt.show()
-  plt.close()
-  
-  ## output of statistics
-  print(pd.DataFrame(pd.Series(eval2.ravel()).describe()).transpose())
-  
-  ## Boxplot using matplotlib
-  fig = plt.figure()
-  ax = fig.add_subplot(1,1,1)
-  
-  bp = ax.boxplot(eval2)
-  ax.set_xticklabels(['Path2'])
-  
-  plt.title('Box plot of Conversion efficiency from Substrate to Target (%)')
-  plt.grid()
-  plt.xlabel('Pathway')
-  plt.ylabel('Conversion efficiency from Substrate to Target (%)')
-  plt.ylim([0,100])
-  
-  plt.savefig("output_boxplot_eval_DDC_plus_DHPAAS_wi_DHPAA_drain_wo_product_inhibition.png")
+  plt.savefig("output_boxplot_eval_solv_10_path2_with_DHPAA_drain.png")
   #plt.show()
   plt.close()
   
   eval = []
 
 
-
-np.savetxt('For_eval_models.csv', data, delimiter=',', header="DDC+MAO-drain+inhibition','DDC+MAO+drain+inhibition','DDC+MAO+drain-inhibition','DDC+DHPAAS-drain+inhibition','DDC+DHPAAS+drain+inhibition','DDC+DHPAAS+drain-inhibition'", fmt='%.5f', comments = '')
+np.savetxt('For_eval_models.csv', data, delimiter=',', header="Eval_index','Eval_index','Eval_index','Eval_index'", fmt='%.5f', comments = '')
 
 ## Boxplot using matplotlib
 fig = plt.figure()
@@ -622,20 +429,19 @@ bp = ap.plot.box(ax=ax)
 
 #bp = ax.boxplot(data)
 #bp = ax.boxplot(csv_input)
-ax.set_xticklabels(['DDC+MAO\n -drain\n+inhibition', 'DDC+MAO\n +drain\n+inhibition', 'DDC+MAO\n +drain\n-inhibition', 'DDC+DHPAAS\n -drain\n+inhibition','DDC+DHPAAS\n +drain\n+inhibition', 'DDC+DHPAAS\n +drain\n-inhibition'])
+ax.set_xticklabels(['DDC+DHPAAS\nno drain\n+inhibition', 'DDC+DHPAAS\n+drain\n+inhibition', 'DDC+DHPAAS\nno drain\nno inhibition', 'DDC+DHPAAS\n+drain\nno inhibition', 'DDC+MAO\n no drain\n+inhibition', 'DDC+MAO\n +drain\n+inhibition', 'DDC+MAO\n +drain\n no inhibition'])
 
 plt.title('Comparison among models\n wi/wo DHPAA drain and product inhibition', fontsize=14)
 #plt.grid()
 plt.xlabel('Model', fontsize=14)
-plt.ylabel('THP Yield (%)', fontsize=14)
+plt.ylabel('Yield (%)', fontsize=14)
 plt.ylim([0,100])
-plt.tick_params(labelsize=14)
-#plt.setp(bp['medians'][0], color='grean', linewidth=2) #Median
+plt.tick_params(labelsize=12)
+##plt.setp(bp['medians'][0], color='red', linewidth=2) #メディアン
 
-plt.setp(ax.get_xticklabels(), rotation=0)
 plt.tight_layout()
 
-plt.savefig("output_boxplot_for_comparison_among_6_models.png")
+plt.savefig("output_boxplot_aaaaa.png")
 
 #plt.show()
 plt.close()
